a8c3b3abf45fede2d693d64832b0d60ef09fd8d1
jehovany04 <7528@holbertonstudents.com>
2024-03-27 21:32:35 +0000

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,8 @@
+#!/bin/bash
+-- script which prepares MySQL server for project
+-- create DB 'hbnb_test_db' and user 'hbnb_test'
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
+USE hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
