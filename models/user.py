80097bad48b7127af317123c0d840bc0f576b668
jehovany04 <7528@holbertonstudents.com>
2024-03-28 22:58:04 +0000

    user.py

--- models/user.py
+++ models/user.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Module for User class."""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """User class to represent a user in the system."""
+    """This class defines a user by various attributes"""
     __tablename__ = 'users'
-
+    id = Column(String(60), primary_key=True)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    def __init__(self, *args, **kwargs):
-        """Initialize User instance."""
-        super().__init__(*args, **kwargs)
-
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
