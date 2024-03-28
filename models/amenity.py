592183969d6029ac23048bc4d0ddeae9f4c3e2f2
jehovany04 <7528@holbertonstudents.com>
2024-03-28 22:54:06 +0000

    models/

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-Base = declarative_base()
 
 class Amenity(BaseModel, Base):
-    """ State Module for HBNB project """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
         "Place", secondary="place_amenity", viewonly=True)
-
