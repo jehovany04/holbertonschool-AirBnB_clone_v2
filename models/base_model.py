bb386d7314437f86041bf23fe1009f7eeda1de10
jehovany04 <7528@holbertonstudents.com>
2024-04-06 12:20:25 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,8 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Instantiates a BaseModel object"""
@@ -32,23 +32,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-
-        # !! WE NEED THIS LINE TO SAVE INTO THE JSON !!
-        # storage.new(self)
-
-        # if not kwargs:
-        #    from models import storage
-        #    self.id = str(uuid.uuid4())
-        #    self.created_at = datetime.now()
-        #    self.updated_at = datetime.now()
-        #    storage.new(self)
-        # else:
-        #    kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-        #                                             '%Y-%m-%dT%H:%M:%S.%f')
-        #    kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-        #                                             '%Y-%m-%dT%H:%M:%S.%f')
-        #    del kwargs['__class__']
-        #    self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -78,3 +61,4 @@ class BaseModel:
     def delete(self):
         """Delete object"""
         models.storage.delete(self)
+
