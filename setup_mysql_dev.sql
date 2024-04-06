5dea786ce22031628c6f17326c473b753263e584
jehovany04 <7528@holbertonstudents.com>
2024-04-06 13:43:33 +0000

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,14 +3,15 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
--- Create table 'users'
+-- Crée la table 'users' avec une colonne 'email'
 USE hbnb_dev_db;
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL -- Ajoute la colonne 'email'
 );
 
--- Flush privileges to apply changes
+-- Flush privileges pour appliquer les modifications
 FLUSH PRIVILEGES;
 
