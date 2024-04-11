14d93e7562292b2b36821d6c46a4955bed963b74
jehovany04 <7528@holbertonstudents.com>
2024-04-11 22:26:19 +0000

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Module that starts a Flask web application"""
+"""Script to start a Flask web application"""
+
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Method that display “Hello HBNB!”"""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Route that displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    # App must be listening on 0.0.0.0, port 5000
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
